b06b5c7adbae99f8cec859c85c6ed38f62cc0215
Neko <nekoislewd@gmail.com>
2018-10-17 00:31:31 +0100

    Setup package

---
+++ pypresence-humanreadable/__init__.py
@@ -0,0 +1,11 @@
+"""
+Set the start or end of an activity through pypresence using
+a human readable date!
+By: LewdNeko
+"""
+
+__title__ = 'pypresence-huamnreadable'
+__author__  = 'QwekoDev'
+__copyright__ = 'Copyright (C) 2018 QwekoDev; Licensed under the MIT License'
+__license__ = 'MIT'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pypresence-humanreadable",
+    version="0.1.0",
+    author="Qweko Dev",
+    author_email="qwekodev@visitourgithub.com",
+    description="A human readable to start / end activity converter for Discord.",
+    url="https://github.com/QwekoDev/pypresence-humanreadable",
+    packages=find_packages()
+)
